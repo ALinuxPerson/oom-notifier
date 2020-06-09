593b352aaab334b5325e88e9a1d1c62c894b6027
Michael Baterna <micheal02052007@gmail.com>
2020-06-09 18:17:23 +0800

    Implement base structure of cli.py:Configuration

--- oom_notifier/__version__.py
+++ oom_notifier/__version__.py
@@ -1,4 +1,4 @@
 from typing import Tuple
 
-VERSION: Tuple[int, ...] = (0, 7, 0)
+VERSION: Tuple[int, ...] = (0, 8, 0)
 __version__: str = '.'.join(map(str, VERSION))
---
+++ oom_notifier/cli.py
@@ -0,0 +1,12 @@
+from typing import Dict
+import oom_notifier
+import configparser
+import platform
+
+class Configuration:
+    def __init__(self, config_directory: str = ""):
+        pass
+
+    @property
+    def _config_location(self):
+        pass
