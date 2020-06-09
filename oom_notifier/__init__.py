c6a6e02847845ffd4d7812e4cd6ddac227fcafbf
Michael Baterna <micheal02052007@gmail.com>
2020-06-09 16:46:09 +0800

    Create oom-notifier:OOM:info()

--- oom_notifier/__init__.py
+++ oom_notifier/__init__.py
@@ -10,6 +10,18 @@ class OOM:
         return [os.path.join(self.proc_path, pid) for pid in os.listdir(self.proc_path) if pid.isdigit()]
 
     @property
+    def info(self) -> Dict[str, int]:
+        ret: Dict[str, int] = {}
+        for path in self._pid_paths:
+            if not os.path.exists(path):
+                continue
+            with open(f"{path}/oom_score", "r") as oom_score:
+                score: int = int([line.strip() for line in oom_score.readlines()][0])
+                ret.update({os.path.basename(path): score})
+
+        return ret
+
+    @property
     def max(self) -> Dict[str, int]:
         pass
 
