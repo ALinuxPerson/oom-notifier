b83157c999806ebd1a9836b7687c3fc5c465358a
Michael Baterna <micheal02052007@gmail.com>
2020-06-09 21:22:51 +0800

    Make property calling not raise an exception when an invalid section is found

--- oom_notifier/utils.py
+++ oom_notifier/utils.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Union
 import configparser
 import platform
 import pathlib
@@ -28,12 +28,23 @@ class Configuration:
         return switch_case.get(os_platform, switch_case["Linux"])
 
     @property
+    def config_dict(self) -> Dict[str, configparser.SectionProxy]:
+        return dict(self.config.items())
+
+    @property
+    def main(self) -> Union[configparser.SectionProxy, Dict[str, str]]:
+        return self.config_dict.get("Main", {
+            "threshold": "1000",
+            "wait_time": "5"
+        })
+
+    @property
     def threshold(self) -> int:
-        return int(self.config["Main"].get("threshold", "1000"))
+        return int(self.main.get("threshold", "1000"))
 
     @property
     def wait_time(self) -> int:
-        return int(self.config["Main"].get("wait_time", "5"))
+        return int(self.main.get("wait_time", "5"))
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.config_directory}')"
