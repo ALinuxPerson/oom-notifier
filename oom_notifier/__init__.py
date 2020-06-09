1500e5928ed784f2781b83398287a67e63ab2fcf
Michael Baterna <micheal02052007@gmail.com>
2020-06-09 16:39:18 +0800

    Create oom-notifier:OOM:_pid_paths()

--- oom_notifier/__init__.py
+++ oom_notifier/__init__.py
@@ -1,8 +1,13 @@
-from typing import Dict
+from typing import Dict, List
+import os
 
 class OOM:
     def __init__(self, proc_path: str = "/proc"):
         self.proc_path = proc_path
+
+    @property
+    def _pid_paths(self) -> List[str]:
+        return [os.path.join(self.proc_path, pid) for pid in os.listdir(self.proc_path) if pid.isdigit()]
 
     @property
     def max(self) -> Dict[str, int]:
