e3bc2c00a1d036b4575674185713d980bdd886c2
Michael Baterna <micheal02052007@gmail.com>
2020-06-09 20:03:15 +0800

    Implement cli.py:Configuration:_config_location()

--- oom_notifier/cli.py
+++ oom_notifier/cli.py
@@ -2,11 +2,21 @@ from typing import Dict
 import oom_notifier
 import configparser
 import platform
+import pathlib
+import os
 
 class Configuration:
     def __init__(self, config_directory: str = ""):
         pass
 
     @property
-    def _config_location(self):
-        pass
+    def _config_location(self) -> str:
+        home_folder: str = str(pathlib.Path.home())
+        os_platform: str = platform.system()
+        switch_case: Dict[str, str] = {
+            "Linux": f"{home_folder}/.config/oom-notifier",
+            "Windows": f"{os.getenv('LOCALAPPDATA')}/oom-notifier",
+            "Darwin": f"{home_folder}/Library/Preferences/oom-notifier"
+        }
+
+        return switch_case.get(os_platform, switch_case["Linux"])
